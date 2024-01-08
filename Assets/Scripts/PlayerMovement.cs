79b9206f462eb2864ffc47765c66e0f7ac9e128d
Arlow5761 <arlow5761@gmail.com>
2024-01-08 13:01:19 +0700

    Adjusted Player Movement, Added Dummy Obstacle

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,9 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(BoxCollider2D)), RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+    public float JumpForce;
     public float Acceleration;
     public float MaxSpeed;
     public int CurrentLane;
@@ -13,11 +14,14 @@ public class PlayerMovement : MonoBehaviour
 
     private float[] LanePositions;
     private BoxCollider2D PlayerCollider;
+    private Rigidbody2D PlayerRb;
     private short LastInput;
+    private bool CanJump;
 
     void Awake()
     {
         PlayerCollider = gameObject.GetComponent<BoxCollider2D>();
+        PlayerRb = gameObject.GetComponent<Rigidbody2D>();
 
         Array.Resize(ref LanePositions, Lanes.Length);
 
@@ -31,6 +35,17 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        // temporary ground checking
+        if (PlayerRb.velocity.y == 0)
+        {
+            CanJump = true;
+        }
+        else
+        {
+            CanJump = false;
+        }
+
+        // Move to the lane above
         if (LastInput == -1)
         {
             if (CurrentLane > 0)
@@ -39,6 +54,7 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
+        // Move to the lane below
         if (LastInput == 1)
         {
             if (CurrentLane < Lanes.Length - 1)
@@ -47,7 +63,19 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
+        // Jump when player can jump
+        if (Input.GetKey(KeyCode.Space) && CanJump)
+        {
+            PlayerRb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+        }
+
         LastInput = 0;
+
+        // Accelerates the player until they hit max speed
+        if (PlayerRb.velocity.x < MaxSpeed)
+        {
+            PlayerRb.AddForce(Vector2.right * Acceleration * PlayerRb.mass);
+        }
     }
 
     void Update()
@@ -65,7 +93,7 @@ public class PlayerMovement : MonoBehaviour
 
     void SwitchLane(int TargetLane)
     {
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x, LanePositions[TargetLane], gameObject.transform.position.z);
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, LanePositions[TargetLane] - LanePositions[CurrentLane] + gameObject.transform.position.y, gameObject.transform.position.z);
         gameObject.layer = Lanes[TargetLane].layer;
         CurrentLane = TargetLane;
     }
