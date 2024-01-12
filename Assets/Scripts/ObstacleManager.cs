eea61246d1b00f1767004009afd3a76afe693043
Bingoimaginations <bingobingoabc@gmail.com>
2024-01-12 20:12:24 +0700

    Added Obstacles

---
+++ Assets/Scripts/ObstacleManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour
+{
+    public GameObject[] Obstacles;
+    public GameObject[] Lanes;
+    public float Step;
+    [Range(0, 1)] public float SpawnChance;
+    public GameObject Target;
+
+    private float CurrentStep;
+
+    void Start()
+    {
+        CurrentStep = Target.transform.position.x;
+
+        GenerateNextStep();
+    }
+
+    void FixedUpdate()
+    {
+        if (Target.transform.position.x - CurrentStep >= Step)
+        {
+            CurrentStep = Target.transform.position.x;
+
+            GenerateNextStep();
+        }
+    }
+
+    void GenerateNextStep()
+    {
+        for (int i = 0; i < Lanes.Length; i++)
+        {
+            if (Random.value <= SpawnChance)
+            {
+                SpawnRandomObstacle(i);
+            }
+        }
+    }
+
+    void SpawnRandomObstacle(int lane)
+    {
+        int ObstacleType = Random.Range(0, Obstacles.Length);
+
+        SpawnObstacle(lane, ObstacleType);
+    }
+
+    void SpawnObstacle(int lane, int index)
+    {
+        GameObject NewObstacle = Instantiate(Obstacles[index], gameObject.transform, true);
+        NewObstacle.transform.position = new Vector3(CurrentStep + Step, Lanes[lane].transform.position.y);
+        //Changing the Layer and Speed (Including the child)
+        SetLayerRecursively(NewObstacle, Lanes[lane].layer, index);
+        static void SetLayerRecursively(GameObject go, int layerNumber, int index)
+        {
+            foreach (Transform trans in go.GetComponentsInChildren<Transform>(true))
+            {
+                //Layer
+                trans.gameObject.layer = layerNumber;
+                if(layerNumber == 8) //Bottom Lane
+                {
+                    trans.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "BottomLane";
+                }
+                if(layerNumber == 7) //Middle Lane
+                {
+                    trans.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "MiddleLane";
+                }
+                if(layerNumber == 6) //Top Lane
+                {
+                    trans.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "TopLane";
+                }
+                //Speed (The rest are still objects, ex. fence)
+                if(index==0) //Car
+                {
+                    trans.gameObject.GetComponent<Rigidbody2D>().velocity=new Vector2(trans.gameObject.GetComponent<Rigidbody2D>().velocity.x-4f, trans.gameObject.GetComponent<Rigidbody2D>().velocity.y);
+                }
+                if(index==1) //Person1
+                {
+                    trans.gameObject.GetComponent<Rigidbody2D>().velocity=new Vector2(trans.gameObject.GetComponent<Rigidbody2D>().velocity.x-2f, trans.gameObject.GetComponent<Rigidbody2D>().velocity.y);
+                }        
+            }
+        }
+    }
+}
