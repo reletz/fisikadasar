af2e2606d48960592aa6725276cc9b88c971d772
Bingoimaginations <bingobingoabc@gmail.com>
2024-01-17 22:32:22 +0700

    Added Main Menu
    - Play Button (Leads to Play Panel)
    - Story Mode (Leads to Story Panel)
    - Options Button (Leads to Volume Panel)
    - Quit Button (Leads to Quitting Game [Only works if it's built into a playing platform])
    
    NOTE
    - Main Menu Background is a Placeholder!
    - Stage Buttons & Endless Button lead to the same scene (until the other scenes are built) (I can handle this tomorrow, but OBSTACLES task still needs to be handed to somebody else.) (Onegaishimasu)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,12 +6,78 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void PlayGame()
+    public GameObject MainMenuPanel;
+    public GameObject PlayPanel;
+    public GameObject StoryPanel;
+    public GameObject OptionsPanel;
+
+    private bool onPlay=false;
+    private bool onStory=false;
+    private bool onOptions=false;
+    // Update is called once per frame
+    void Update()
     {
-        SceneManager.LoadSceneAsync(1);
+        //Esc key in Options to go back to Main Menu
+        if(Input.GetKeyDown(KeyCode.Escape) && onPlay)
+        {
+            Backfrom_Play();
+        }
+        if(Input.GetKeyDown(KeyCode.Escape) && onStory)
+        {
+            Backfrom_Story();
+        }
+        if(Input.GetKeyDown(KeyCode.Escape) && onOptions)
+        {
+            Backfrom_Options();
+        }
     }
 
-    public void Quit()
+    //Here are the functions to be called by the buttons
+    public void PLACEHOLDER_Play()
+    {
+        onPlay=false;
+        onStory=false;
+        AudioManager.Instance.StopMusic("BGM");
+        PlayPanel.SetActive(false);
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void Play()
+    {
+        onPlay=true;
+        PlayPanel.SetActive(true);
+    }
+    public void Backfrom_Play()
+    {
+        onPlay=false;
+        PlayPanel.SetActive(false);
+    }
+
+    public void Story()
+    {
+        onPlay=false;
+        onStory=true;
+        PlayPanel.SetActive(false);
+        StoryPanel.SetActive(true);
+    }
+    public void Backfrom_Story()
+    {
+        onPlay=true;
+        onStory=false;
+        PlayPanel.SetActive(true);
+        StoryPanel.SetActive(false);
+    }
+    public void Options()
+    {
+        onOptions=true;
+        OptionsPanel.SetActive(true);
+    }
+    public void Backfrom_Options()
+    {
+        onOptions=false;
+        OptionsPanel.SetActive(false);
+    }
+    
+    public void QuitGame()
     {
         Application.Quit();
     }
